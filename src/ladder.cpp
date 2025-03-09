54471d3a6b79d314f2952d68ce064985053831d7
vatsag-06 <vatsag@uci.edu>
2025-03-09 04:46:12 +0000

    More errors.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -99,6 +99,7 @@ void print_word_ladder(const vector<string> &ladder) {
         cout << "No word ladder found.";
     }
     else{
+        cout << "Word ladder found: ";
         for (int i = 0; i < ladder.size(); i++) {
             cout << ladder[i] << " ";
         }
